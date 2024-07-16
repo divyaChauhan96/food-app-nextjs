03d1d6fab9fe59b3aec101f7b5b016f95beddfe4
Divya <divya2309c@gmail.com>
2024-07-16 16:47:08 +0530

    Meal details page completed

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+//every component receives props from next JS
+// props => params => [mealSlug] to access the meal path details
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    // next navigation method that calls the nearest not found page
+    notFound();
+    //This function stops the execution of the Page below
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
