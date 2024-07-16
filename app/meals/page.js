cbe0dd03bf673abbc2f5ad9959f8724374c97026
Divya <divya2309c@gmail.com>
2024-07-16 13:22:07 +0530

    Added browse meals page with default data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meal-grid";
+import { getMeals } from "@/lib/meals";
+
 export default function MealsPage() {
+  const mealsList = getMeals();
   return (
-    <main>
-      <h1>Browse Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={mealsList} />
+      </main>
+    </>
   );
 }
